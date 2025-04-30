17808a759e99730477dcaa13f749dc8b77fb2f18
Coșa Andrei Valeriu <42703353+AndreiOp235@users.noreply.github.com>
2025-04-30 19:46:27 +0300

    added filtering

--- Scripts/reverse3.py
+++ Scripts/reverse3.py
@@ -1,7 +1,18 @@
 import serial
+import sys
 
-ser = serial.Serial('COM11', 9600, timeout=1)
+print("This is the name of the program:", sys.argv[0])
+
+if len(sys.argv) > 1:
+    lungime = int(sys.argv[1])
+    print("Argument List:", str(sys.argv))
+    print(sys.argv[1])
+else:
+    lungime = 1
+
+ser = serial.Serial('COM15', 9600, timeout=1)
 print("Listening on COM22...")
+stiva=[]
 
 def parse_signed_24bit(b):
     val = b[0] << 16 | b[1] << 8 | b[2]
@@ -10,6 +21,7 @@ def parse_signed_24bit(b):
     return val
 
 try:
+    
     while True:
         if ser.in_waiting >= 7:
             buffer = ser.read(7)
@@ -17,10 +29,16 @@ try:
                 print(f"Warning: First byte is {buffer[0]}, expected 12. Dropping packet.")
                 continue
 
+            if(len(stiva)==lungime):
+                stiva.pop(0)
+                
             v1 = parse_signed_24bit(buffer[1:4])
             v1=v1/1000.0
+
             i1 = parse_signed_24bit(buffer[4:7])
             i1=i1/1000.0
+            stiva.append(i1)
+            i1 = sum(stiva) / len(stiva)
 
             print(f"Voltage (v1): {v1:.3f}, Current (i1): {i1:.3f}")
 except KeyboardInterrupt:
